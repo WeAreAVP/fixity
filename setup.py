9e07bae1237222918a9d1af339367d2e5d24eb23
Furqan <furqan@avpreserve.com>
2014-04-05 01:46:43 +0500

    Adding Images to resources Fix-93

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 APP = ['Fixity.py']
 DATA_FILES = []
-OPTIONS = {'argv_emulation': True,  'iconfile':'logo_sign_trans.icns','resources':'bin,schedules,reports,history,debug'}
+OPTIONS = {'argv_emulation': True,  'iconfile':'logo_sign_trans.icns','resources':'bin,schedules,reports,history,debug,images'}
 setup(
     app=APP,
     data_files=DATA_FILES,
