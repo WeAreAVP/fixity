e627c15128036580cf9269ada5c5a2c9c06ada29
Furqan Wasi <furqan@avpreserve.com>
2014-04-17 18:28:25 +0500

    fixing spelling mistakes

--- AboutFixity.py
+++ AboutFixity.py
@@ -104,9 +104,9 @@ class AboutFixity(QDialog):
         #Definitions
         LicenseText += '<ol>'
         LicenseText += '    <li>'
-        LicenseText += '        <p> "License" shall mean the terms and conditions for use, reproduction,and distribution as defined by Sections 1 through 9 of this document.</p>'
+        LicenseText += '        <p> "License" shall mean the terms and conditions for use, reproduction, and distribution as defined by Sections 1 through 9 of this document.</p>'
         LicenseText += '        <p> "Licensor" shall mean the copyright owner or entity authorized by the copyright owner that is granting the License.</p>'
-        LicenseText += '        <p> "Legal Entity" shall mean the union of the acting entity and all other entities that control, are controlled by, or are under common control with that entity. For the purposes of this definition,"control" means (i) the power, direct or indirect, to cause thedirection or management of such entity, whether by contract orotherwise, or (ii) ownership of fifty percent (50%) or more of the outstanding shares, or (iii) beneficial ownership of such entity.</p>'
+        LicenseText += '        <p> "Legal Entity" shall mean the union of the acting entity and all other entities that control, are controlled by, or are under common control with that entity. For the purposes of this definition, "control" means (i) the power, direct or indirect, to cause the direction or management of such entity, whether by contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the outstanding shares, or (iii) beneficial ownership of such entity.</p>'
         LicenseText += '        <p> "You" (or "Your") shall mean an individual or Legal Entity exercising permissions granted by this License.</p>'
         LicenseText += '        <p> "Source" form shall mean the preferred form for making modifications, including but not limited to software source code, documentation source, and configuration files.</p>'
         LicenseText += '        <p> "Object" form shall mean any form resulting from mechanical transformation or translation of a Source form, including but not limited to compiled object code, generated documentation, and conversions to other media types.</p>'
