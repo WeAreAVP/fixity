b4d7d9d57013f44dcfd17baf05221e22a95316b1
Furqan Wasi <furqan@avpreserve.com>
2014-05-06 18:33:30 +0500

    Ticket Fix - 131

--- FixityCore.py
+++ FixityCore.py
@@ -602,9 +602,9 @@ def writer(algoUsed, projectPath, TotalFilesScanned, confirmedFileScanned , move
                 os.mkdir( str(createPath) + 'reports' )
             except Exception as Excep:
                 print(Excep[0])
-        if not fromDycruptionClass
+        if not fromDycruptionClass:
             rn = str(pathInfo)+'reports'+str(os.sep)+'fixity_' + str(datetime.date.today()) + '-' + str(datetime.datetime.now().strftime('%H%M%S')) + '_' + str(NameOfFile[(len(NameOfFile)-1)])  + '.tsv'
-        except:
+        else:
             rn = str(pathInfo)+'reports'+str(os.sep)+'fixity_' + str(datetime.date.today()) + '-' + str(datetime.datetime.now().strftime('%H%M%S')) + '_' + str(NameOfFile)  + '.tsv'
         try:
             rn = str(rn).replace(' ', '\\ ')
