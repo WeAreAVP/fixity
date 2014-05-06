9b5d1e75168c3489f48f169c9bb92da161366fe7
Furqan Wasi <furqan@avpreserve.com>
2014-05-06 18:32:38 +0500

    Ticket Fix - 131

--- FixityCore.py
+++ FixityCore.py
@@ -583,12 +583,13 @@ def writer(algoUsed, projectPath, TotalFilesScanned, confirmedFileScanned , move
         AutiFixPath = (getcwd()).replace('schedules','').replace('\\\\',"\\")
         rn = AutiFixPath+str(os.sep)+'reports'+str(os.sep)+'fixity_' + str(datetime.date.today()) + '-' + str(datetime.datetime.now().strftime('%H%M%S')) + '_' + str(projectName[0])  + '.tsv'
     else:
-
+        fromDycruptionClass = False
         AutiFixPath = (getcwd()).replace('schedules','').replace('//',"/")
         try:
             NameOfFile = str(projectName[1]).split('/')
         except:
             NameOfFile = str(projectName[0])
+            fromDycruptionClass = True
         print(NameOfFile)
         
 
@@ -601,7 +602,7 @@ def writer(algoUsed, projectPath, TotalFilesScanned, confirmedFileScanned , move
                 os.mkdir( str(createPath) + 'reports' )
             except Exception as Excep:
                 print(Excep[0])
-        try:
+        if not fromDycruptionClass
             rn = str(pathInfo)+'reports'+str(os.sep)+'fixity_' + str(datetime.date.today()) + '-' + str(datetime.datetime.now().strftime('%H%M%S')) + '_' + str(NameOfFile[(len(NameOfFile)-1)])  + '.tsv'
         except:
             rn = str(pathInfo)+'reports'+str(os.sep)+'fixity_' + str(datetime.date.today()) + '-' + str(datetime.datetime.now().strftime('%H%M%S')) + '_' + str(NameOfFile)  + '.tsv'
