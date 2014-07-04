2c0c1967b0eff557f665a9b728f2f5733cfd4cea
Furqan <furqan@avpreserve.com>
2014-07-04 13:50:38 +0500

    add unit test for specail characters

--- test/TestSuite.py
+++ test/TestSuite.py
@@ -39,6 +39,7 @@ class TestSuite(unittest.TestCase):
         pass
     #
     def testRunAlgorithmTestCases(self):
+
         print('==================================================================')
         print(' Running Alogorithm for Verification Test Cases ')
         print('==================================================================')
@@ -76,7 +77,6 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(response_test_confirm_file_delete[0], response_test_confirm_file_delete[1], response_test_confirm_file_delete[2])
 
         # ================================== Special Character Testing ================================
-        #
         # Confirmed  FileExists::YES   ||SameHashOfFile::YES    ||SameFilePath::YES    ||SameI-Node::NO
         response_test_confirm_file = self.algorithm_test_case.test_confirm_file(True)
         self.assertEqual(response_test_confirm_file[0], response_test_confirm_file[1], response_test_confirm_file[2])
@@ -110,28 +110,36 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(response_test_confirm_file_delete[0], response_test_confirm_file_delete[1], response_test_confirm_file_delete[2])
 
     def testProjectTestCases(self):
+
         print('==================================================================')
         print(' Running Projects Operation Test Cases')
         print('==================================================================')
 
+        # Project Run Unit test
         response_test_run_project = self.project_test_case.run_project(self.Fixtures.project_name)
         self.assertEqual(response_test_run_project[0], response_test_run_project[1], response_test_run_project[2])
 
+        # Project Run Unit test
         response_test_delete_project = self.project_test_case.delete_project(self.Fixtures.project_name)
         self.assertEqual(response_test_delete_project[0], response_test_delete_project[1], response_test_delete_project[2])
 
+        # Change Project Name Unit test
         response_test_change_project_name = self.project_test_case.change_project_name(self.Fixtures.project_name, 'testing')
         self.assertEqual(response_test_change_project_name[0], response_test_change_project_name[1], response_test_change_project_name[2])
 
+        # Save Project Unit test
         response_test_save_project = self.project_test_case.save_project(self.Fixtures.project_name)
         self.assertEqual(response_test_save_project[0], response_test_save_project[1], response_test_save_project[2])
 
+        # Change Algorithm Unit test
         response_test_change_algorithm = self.project_test_case.change_algorithm(self.Fixtures.project_name)
         self.assertEqual(response_test_change_algorithm[0], response_test_change_algorithm[1], response_test_change_algorithm[2])
 
+        # Filter scanned files Unit test
         response_test_filters_files = self.project_test_case.filters_files(self.Fixtures.project_name)
         self.assertEqual(response_test_filters_files[0], response_test_filters_files[1], response_test_filters_files[2])
 
+        # Import Project Unit test
         response_test_import_project = self.project_test_case.import_project('Testing')
         self.assertEqual(response_test_import_project[0], response_test_import_project[1], response_test_import_project[2])
 
@@ -141,12 +149,15 @@ class TestSuite(unittest.TestCase):
         print(' Running Email Notification Test Cases')
         print('==================================================================')
 
+        # Testing Email Unit Test
         response_of_email = self.email_test_case.test_testing_email()
         self.assertEqual(response_of_email[0], response_of_email[1], response_of_email[2])
 
+        # Attachment Email Unit Test
         response_of_email_attachment = self.email_test_case.test_attachment_email()
         self.assertEqual(response_of_email_attachment[0], response_of_email_attachment[1], response_of_email_attachment[2])
 
+        # Error Email Unit Test
         response_of_email_error = self.email_test_case.test_Error_email()
         self.assertEqual(response_of_email_error[0], response_of_email_error[1], response_of_email_error[2])
         pass
