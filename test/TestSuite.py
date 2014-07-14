c16fe7bbcbfd9d0e5b900add30bdb199aaaa2d6d
Furqan <furqan@avpreserve.com>
2014-07-14 13:56:52 +0500

    Fixing Problem in Fix-208,Fix-210

--- test/TestSuite.py
+++ test/TestSuite.py
@@ -138,97 +138,97 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(response_test_change_base_path[0], response_test_change_base_path[1], response_test_change_base_path[2])
 
 
-    ## Running Projects Operation Test Cases
-    #def testBProjectTestCases(self):
-    #
-    #    print('==================================================================')
-    #    print(' Running Projects Operation Test Cases')
-    #    print('==================================================================')
-    #
-    #    # Project Run Unit test
-    #    response_test_run_project = self.project_test_case.run_project(self.Fixtures.project_name)
-    #    self.assertEqual(response_test_run_project[0], response_test_run_project[1], response_test_run_project[2])
-    #
-    #    # Project Run Unit test
-    #    response_test_delete_project = self.project_test_case.delete_project(self.Fixtures.project_name)
-    #    self.assertEqual(response_test_delete_project[0], response_test_delete_project[1], response_test_delete_project[2])
-    #
-    #    # Change Project Name Unit test
-    #    response_test_change_project_name = self.project_test_case.change_project_name(self.Fixtures.project_name, 'testing')
-    #    self.assertEqual(response_test_change_project_name[0], response_test_change_project_name[1], response_test_change_project_name[2])
-    #
-    #    # Save Project Unit test
-    #    response_test_save_project = self.project_test_case.save_project(self.Fixtures.project_name)
-    #    self.assertEqual(response_test_save_project[0], response_test_save_project[1], response_test_save_project[2])
-    #
-    #    # Change Algorithm Unit test
-    #    response_test_change_algorithm = self.project_test_case.change_algorithm(self.Fixtures.project_name)
-    #    self.assertEqual(response_test_change_algorithm[0], response_test_change_algorithm[1], response_test_change_algorithm[2])
-    #
-    #    # Filter scanned files Unit test
-    #    response_test_filters_files = self.project_test_case.filters_files(self.Fixtures.project_name)
-    #    self.assertEqual(response_test_filters_files[0], response_test_filters_files[1], response_test_filters_files[2])
-    #
-    #    # Import Project Unit test
-    #    response_test_import_project = self.project_test_case.import_project('Testing')
-    #    self.assertEqual(response_test_import_project[0], response_test_import_project[1], response_test_import_project[2])
+    # Running Projects Operation Test Cases
+    def testBProjectTestCases(self):
 
-    ## Running Email Notification Test Cases
-    #def testCEmailNotification(self):
-    #
-    #    print('==================================================================')
-    #    print(' Running Email Notification Test Cases')
-    #    print('==================================================================')
-    #
-    #    # Testing Email Unit Test
-    #    response_of_email = self.email_test_case.test_testing_email()
-    #    self.assertEqual(response_of_email[0], response_of_email[1], response_of_email[2])
-    #
-    #    # Attachment Email Unit Test
-    #    response_of_email_attachment = self.email_test_case.test_attachment_email()
-    #    self.assertEqual(response_of_email_attachment[0], response_of_email_attachment[1], response_of_email_attachment[2])
-    #
-    #    # Error Email Unit Test
-    #    response_of_email_error = self.email_test_case.test_Error_email()
-    #    self.assertEqual(response_of_email_error[0], response_of_email_error[1], response_of_email_error[2])
-    #    pass
+        print('==================================================================')
+        print(' Running Projects Operation Test Cases')
+        print('==================================================================')
+
+        # Project Run Unit test
+        response_test_run_project = self.project_test_case.run_project(self.Fixtures.project_name)
+        self.assertEqual(response_test_run_project[0], response_test_run_project[1], response_test_run_project[2])
+
+        # Project Run Unit test
+        response_test_delete_project = self.project_test_case.delete_project(self.Fixtures.project_name)
+        self.assertEqual(response_test_delete_project[0], response_test_delete_project[1], response_test_delete_project[2])
+
+        # Change Project Name Unit test
+        response_test_change_project_name = self.project_test_case.change_project_name(self.Fixtures.project_name, 'testing')
+        self.assertEqual(response_test_change_project_name[0], response_test_change_project_name[1], response_test_change_project_name[2])
+
+        # Save Project Unit test
+        response_test_save_project = self.project_test_case.save_project(self.Fixtures.project_name)
+        self.assertEqual(response_test_save_project[0], response_test_save_project[1], response_test_save_project[2])
+
+        # Change Algorithm Unit test
+        response_test_change_algorithm = self.project_test_case.change_algorithm(self.Fixtures.project_name)
+        self.assertEqual(response_test_change_algorithm[0], response_test_change_algorithm[1], response_test_change_algorithm[2])
+
+        # Filter scanned files Unit test
+        response_test_filters_files = self.project_test_case.filters_files(self.Fixtures.project_name)
+        self.assertEqual(response_test_filters_files[0], response_test_filters_files[1], response_test_filters_files[2])
+
+        # Import Project Unit test
+        response_test_import_project = self.project_test_case.import_project('Testing')
+        self.assertEqual(response_test_import_project[0], response_test_import_project[1], response_test_import_project[2])
+
+    # Running Email Notification Test Cases
+    def testCEmailNotification(self):
+
+        print('==================================================================')
+        print(' Running Email Notification Test Cases')
+        print('==================================================================')
+
+        # Testing Email Unit Test
+        response_of_email = self.email_test_case.test_testing_email()
+        self.assertEqual(response_of_email[0], response_of_email[1], response_of_email[2])
+
+        # Attachment Email Unit Test
+        response_of_email_attachment = self.email_test_case.test_attachment_email()
+        self.assertEqual(response_of_email_attachment[0], response_of_email_attachment[1], response_of_email_attachment[2])
+
+        # Error Email Unit Test
+        response_of_email_error = self.email_test_case.test_Error_email()
+        self.assertEqual(response_of_email_error[0], response_of_email_error[1], response_of_email_error[2])
+        pass
 
 
-    #def testDRequiredFilesAndDirsCreation(self):
-    #
-    #    print('==================================================================')
-    #    print(' Running Required Files And Dirs Creation Test Cases')
-    #    print('==================================================================')
-    #
-    #    # Is Config File Exists
-    #    response_of_is_config_file_exists = self.requireds_creation_test_case.is_config_file_exists()
-    #
-    #    self.assertEqual(response_of_is_config_file_exists[0], response_of_is_config_file_exists[1], response_of_is_config_file_exists[2])
-    #
-    #    # Is Database File Exists
-    #    response_of_is_database_file_exists = self.requireds_creation_test_case.is_database_file_exists()
-    #    self.assertEqual(response_of_is_database_file_exists[0], response_of_is_database_file_exists[1], response_of_is_database_file_exists[2])
-    #
-    #    # Is Debug File Exists
-    #    response_of_is_debug_files_exists = self.requireds_creation_test_case.is_debug_files_exists()
-    #    self.assertEqual(response_of_is_debug_files_exists[0], response_of_is_debug_files_exists[1], response_of_is_debug_files_exists[2])
-    #
-    #    # Is History Directory Exists
-    #    response_of_is_history_dir_exists = self.requireds_creation_test_case.is_history_dir_exists()
-    #    self.assertEqual(response_of_is_history_dir_exists[0], response_of_is_history_dir_exists[1], response_of_is_history_dir_exists[2])
-    #
-    #    # Is Reports Directory Exists
-    #    response_of_is_report_dir_exists = self.requireds_creation_test_case.is_report_dir_exists()
-    #    self.assertEqual(response_of_is_report_dir_exists[0], response_of_is_report_dir_exists[1], response_of_is_report_dir_exists[2])
-    #
-    #    # Is Schedulers Directory Exists
-    #    response_of_is_schedules_dir_exists = self.requireds_creation_test_case.is_schedules_dir_exists()
-    #    self.assertEqual(response_of_is_schedules_dir_exists[0], response_of_is_schedules_dir_exists[1], response_of_is_schedules_dir_exists[2])
+    def testDRequiredFilesAndDirsCreation(self):
+
+        print('==================================================================')
+        print(' Running Required Files And Dirs Creation Test Cases')
+        print('==================================================================')
+
+        # Is Config File Exists
+        response_of_is_config_file_exists = self.requireds_creation_test_case.is_config_file_exists()
+
+        self.assertEqual(response_of_is_config_file_exists[0], response_of_is_config_file_exists[1], response_of_is_config_file_exists[2])
+
+        # Is Database File Exists
+        response_of_is_database_file_exists = self.requireds_creation_test_case.is_database_file_exists()
+        self.assertEqual(response_of_is_database_file_exists[0], response_of_is_database_file_exists[1], response_of_is_database_file_exists[2])
+
+        # Is Debug File Exists
+        response_of_is_debug_files_exists = self.requireds_creation_test_case.is_debug_files_exists()
+        self.assertEqual(response_of_is_debug_files_exists[0], response_of_is_debug_files_exists[1], response_of_is_debug_files_exists[2])
+
+        # Is History Directory Exists
+        response_of_is_history_dir_exists = self.requireds_creation_test_case.is_history_dir_exists()
+        self.assertEqual(response_of_is_history_dir_exists[0], response_of_is_history_dir_exists[1], response_of_is_history_dir_exists[2])
+
+        # Is Reports Directory Exists
+        response_of_is_report_dir_exists = self.requireds_creation_test_case.is_report_dir_exists()
+        self.assertEqual(response_of_is_report_dir_exists[0], response_of_is_report_dir_exists[1], response_of_is_report_dir_exists[2])
+
+        # Is Schedulers Directory Exists
+        response_of_is_schedules_dir_exists = self.requireds_creation_test_case.is_schedules_dir_exists()
+        self.assertEqual(response_of_is_schedules_dir_exists[0], response_of_is_schedules_dir_exists[1], response_of_is_schedules_dir_exists[2])
 
 
     def tearDown(self):
         print('Tear Down!')
-        #self.Fixtures.delete_testing_data()
+        self.Fixtures.delete_testing_data()
         print('')
         pass
 
