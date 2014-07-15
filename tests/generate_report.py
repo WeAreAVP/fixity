c8c174f54e61e9ebf153c62d8dbef81b972fa36b
Furqan Wasi <furqan@avpreserve.com>
2014-07-15 17:36:08 +0500

    Done Fix - 218 on for mac Create Coverage report for Mac

--- tests/generate_report.py
+++ tests/generate_report.py
@@ -1,7 +1,22 @@
-import subprocess
+import shlex, subprocess, os
+# Constructor
 
-startupinfo = subprocess.STARTUPINFO()
-startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+if os.name == 'posix':
+    OsType = 'linux'
+
+elif os.name == 'nt':
+    OsType = 'Windows'
+
+elif os.name == 'os2':
+    OsType = 'check'
 
 command_html_report = 'coverage html --omit=../GUI/*,../UnitTest/*,../Config/Validation.py,../Core/CustomException.py,../Core/Debugger.py'
-process_response = subprocess.call(command_html_report, startupinfo=startupinfo)
+if OsType == 'Windows':
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    process_response = subprocess.call(command_html_report, startupinfo=startupinfo)
+else:
+
+    args = shlex.split(command_html_report)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE)
